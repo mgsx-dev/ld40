cf4de747d5450dc8b55293b2ea45d4e935e4b959
Germain AUBERT <germain.aubert@gmail.com>
2017-12-02 04:39:07 +0100

    init from template

---
+++ assets/README.md
@@ -0,0 +1,9 @@
+
+    $ ./gralew assets:editFont_name
+
+    $ ./gralew assets:clean assets:updateAssets
+    
+    
+Install font for inkscape : 
+	
+	$ cp assets/ferrum.otf ~/.fonts/
---
+++ desktop/src/net/mgsx/ld40/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.ld40;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.ld40.LD40;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD40(), config);
+	}
+}
